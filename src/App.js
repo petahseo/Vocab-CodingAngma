8dde8c7bfc59fb1a497246fcc6d0aa5083beb53b
petah <petahseo@gmail.com>
2022-08-25 16:25:52 +0900

    router, usestate, useparams, props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="/*" element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,18 +1,18 @@
 import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
+import Word from "./Word";
 
 export default function Day() {
-  const day = 1;
-  const wordList = dummy.words.filter((word) => word.day === day);
+  const { day } = useParams();
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
     <>
+      <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map((word) => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+            <Word word={word} key={word.id} />
           ))}
         </tbody>
       </table>
